63e745862f612581320a3fb3ed81887ceb1934f3
farizdotid <contact.farizramadhan@gmail.com>
2024-03-15 23:33:55 +0700

    first commit :tada:

---
+++ src/types.ts
@@ -0,0 +1,21 @@
+export interface CompatibilityItem {
+  icon: string;
+  title: string;
+  url: string;
+}
+
+export interface FeatureItem {
+  description: string;
+  icon: string;
+}
+
+export interface FooterLink {
+  description: string;
+  icon: string;
+  url: string;
+}
+
+export interface NavItem {
+  title: string;
+  url: string;
+}
